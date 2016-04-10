5b199e769ee0788811590932a829d7f5f518ef76
chambana <akwatson@comcast.net>
2016-04-09 23:42:37 -0400

    scaled Rpi cam grab from 5 megapixels down to 320x240

--- piratebox/piratebox/bin/camera_controller.py
+++ piratebox/piratebox/bin/camera_controller.py
@@ -2,5 +2,5 @@ import subprocess
 import time
 
 def run_camera_controller():
-    subprocess.Popen("/opt/vc/bin/raspistill -o /opt/piratebox/share/Shared/drone_pic.jpg -t 21600000 -tl 1000", shell=True)
+    subprocess.Popen("/opt/vc/bin/raspistill -o /opt/piratebox/www/drone.jpg -t 21600000 -tl 1000 -w 320 -h 240", shell=True)
 
